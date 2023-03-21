2841a4e3c66a7baeaa2cc43d06172e4dab310f3c
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-03-21 13:58:39 +0300

    lesson 21 page Dialogs HTML coding

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,11 +1,22 @@
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
-}
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Alina</div>
+        <div className={s.dialog}>Slava</div>
+        <div className={s.dialog}>Karolina</div>
+        <div className={s.dialog}>Vlad</div>
+        <div className={s.dialog}>Anna</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi!</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>What's up!</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
