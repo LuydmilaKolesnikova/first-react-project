82e6daced0cc52e05706920abd86fd771aa82002
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-03-22 16:05:59 +0300

    lesson 25 map

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -15,29 +15,28 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
-  let dialogsData = [
+  let dialogs = [
     { id: "1", name: "Alina" },
     { id: "2", name: "Slava" },
     { id: "3", name: "Karolina" },
     { id: "4", name: "Vlad" },
     { id: "5", name: "Anna" },
   ];
-  let messagesData = [
+  let messages = [
     { id: "1", message: "Hi!" },
     { id: "2", message: "How are you?" },
     { id: "3", message: "What's up!" },
   ];
 
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
+
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogsItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-      </div>
-      <div className={s.messages}>
-        <Message message={messagesData[0].message} />
-        <Message message={messagesData[1].message} />
-      </div>
+      <div className={s.dialogsItems}>{dialogsElements}</div>
+      <div className={s.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/Posts/Posts.js
+++ src/components/Profile/Posts/Posts.js
@@ -2,10 +2,14 @@ import s from "./Posts.module.css";
 import Post from "./Post/Post";
 
 const Posts = () => {
-  let postsData = [
+  let posts = [
     { id: "1", message: "Hi!", likesCount: "0" },
     { id: "2", message: "How are you?", likesCount: "23" },
   ];
+
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
   return (
     <div className={s.postsBlock}>
@@ -16,16 +20,7 @@ const Posts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        <Post
-          message={postsData[0].message}
-          likesCount={postsData[0].likesCount}
-        />
-        <Post
-          message={postsData[1].message}
-          likesCount={postsData[1].likesCount}
-        />
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
