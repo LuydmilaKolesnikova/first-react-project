58923e837b5842f02cc6f6498cbb977334b9364d
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-03-23 13:12:47 +0300

    lesson 28 replace data to index.js

--- src/App.js
+++ src/App.js
@@ -9,24 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-const App = () => {
-  let posts = [
-    { id: "1", message: "Hi!", likesCount: "11" },
-    { id: "2", message: "How are you?", likesCount: "23" },
-  ];
-  let dialogs = [
-    { id: "1", name: "Alina" },
-    { id: "2", name: "Slava" },
-    { id: "3", name: "Karolina" },
-    { id: "4", name: "Vlad" },
-    { id: "5", name: "Anna" },
-  ];
-  let messages = [
-    { id: "1", message: "Hi!" },
-    { id: "2", message: "How are you?" },
-    { id: "3", message: "What's up!" },
-  ];
-
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -36,9 +19,14 @@ const App = () => {
           <Routes>
             <Route
               path="/dialogs/*"
-              element={<Dialogs dialogs={dialogs} messages={messages} />}
+              element={
+                <Dialogs dialogs={props.dialogs} messages={props.messages} />
+              }
             ></Route>
-            <Route path="/profile" element={<Profile posts={posts} />}></Route>
+            <Route
+              path="/profile"
+              element={<Profile posts={props.posts} />}
+            ></Route>
             <Route path="/news" element={<News />}></Route>
             <Route path="/music" element={<Music />}></Route>
             <Route path="/settings" element={<Settings />}></Route>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let posts = [
+  { id: "1", message: "Hi!", likesCount: "11" },
+  { id: "2", message: "How are you?", likesCount: "23" },
+];
+let dialogs = [
+  { id: "1", name: "Alina" },
+  { id: "2", name: "Slava" },
+  { id: "3", name: "Karolina" },
+  { id: "4", name: "Vlad" },
+  { id: "5", name: "Anna" },
+];
+let messages = [
+  { id: "1", message: "Hello!" },
+  { id: "2", message: "How are you?" },
+  { id: "3", message: "What's up!" },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>
 );
 
