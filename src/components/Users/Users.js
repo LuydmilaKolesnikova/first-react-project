1cbe748bc6de3f6021c46050169d9f3df6fd12d8
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-04-11 12:38:42 +0300

    lesson 53 class Users extends React.Component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,65 @@
+import React from "react";
 import s from "./Users.module.css";
-import UserItem from "./UserItem/UserItem";
 import axios from "axios";
 import avatar_default from "../../assets/images/avatar_default.jpeg";
 
-const Users = (props) => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id} className={s.userItem}>
+            <div className={s.image}>
+              <img
+                className={s.avatar}
+                src={u.avatar ? u.avatar : avatar_default}
+              />
+              <div>
+                {u.followed ? (
+                  <button
+                    className={s.followBtn}
+                    onClick={() => this.props.unfollowUser(u.id)}
+                  >
+                    UNFOLLOW
+                  </button>
+                ) : (
+                  <button
+                    className={s.followBtn}
+                    onClick={() => this.props.followUser(u.id)}
+                  >
+                    FOLLOW
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className={s.info}>
+              <div>
+                <div className={s.name}>{u.name}</div>
+                <div className={s.status}>{u.status}</div>
+              </div>
+              <div className={s.location}>
+                <div className={s.country}>{u.country}</div>
+                <div className={s.city}>{u.city}</div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+/* const Users = (props) => {
   if (props.users.length === 0) {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
@@ -54,6 +110,6 @@ const Users = (props) => {
       ))}
     </div>
   );
-};
+}; */
 
 export default Users;
