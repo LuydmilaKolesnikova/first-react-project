89832a834f81c0b93c108a214fa5fcc885e7e9a7
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-05-11 16:16:23 +0300

    add domain name on https://social-network.samuraijs.com/account

--- src/api/instanceAPI.js
+++ src/api/instanceAPI.js
@@ -4,6 +4,8 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
   withCredentials: true,
   headers: {
-    "API-KEY": "e079df41-01a2-4c78-a3d3-a9116a48cff8",
+    "API-KEY": "0b5bf0f5-fd4a-492c-9baf-6d44abe41fc0",
   },
 });
+
+//e079df41-01a2-4c78-a3d3-a9116a48cff8
